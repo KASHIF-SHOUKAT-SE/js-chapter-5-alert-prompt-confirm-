ed48211fc2b95330f505ace95f7a5afef2a02e67
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-28 11:16:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("hi there i am here")
+let a = prompt("enter here a value", "857")
+a= Number.parseInt(a)
+alert("you enter a type" +(typeof a))
+let write = confirm("do you want to allow me ")
+if(write){
+    document.write(a)
+}
+else{
+document.write("do you not want me allow")
+}
